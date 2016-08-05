47c7fec7bed48c7ce8cf599358d573e8b466cd6b
SergeyKotyushkin <sergey.kotyushkin@firstlinesoftware.com>
2016-08-05 09:30:27 +0300

    Fix layout of the chat page

---
+++ Chat.Web/Scripts/chat.js
@@ -0,0 +1,20 @@
+﻿$(document).ready(function () {
+    
+    function chatViewModel() {
+        var self = this;
+
+        self.currentChat = ko.observable(null);
+
+        self.chats = ko.observableArray([]);
+
+        self.users = ko.observableArray([]);
+    }
+
+    var viewModel = new chatViewModel();
+    ko.applyBindings(viewModel);
+
+
+    // create hub
+    //var chat = $.connection.chatHub;
+
+});
